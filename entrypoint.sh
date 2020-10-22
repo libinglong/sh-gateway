aa248358b3595e4bdca62f198908a28210e1121c
libinglong <libinglong9@gmail.com>
2020-10-22 19:09:55 +0800

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 set -e
-logfile=gc.log.$(date "+%Y-%m-%d.%H-%M-%S")
+log_home=/opt/logs
+mkdir -p ${log_home}
 JAVA_OPTS="
 -Xms2G -Xmx2G -server
+-XX:+HeapDumpOnOutOfMemoryError
+-XX:+ParallelRefProcEnabled
+-XX:+DisableExplicitGC
 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC
--Xloggc:${logfile}
--XX:+PrintGC
--XX:+PrintReferenceGC
--XX:+PrintHeapAtGC
--XX:+PrintGCDetails
--XX:+PrintGCTimeStamps
--XX:+PrintGCDateStamps
--XX:+PrintAdaptiveSizePolicy
--XX:+PrintGCApplicationStoppedTime
--XX:+PrintGCApplicationConcurrentTime
--XX:+PrintTenuringDistribution
--XX:+SafepointTimeout
--XX:SafepointTimeoutDelay=1000
--XX:+PrintSafepointStatistics
--XX:PrintSafepointStatisticsCount=1
+-Xlog:safepoint,classhisto*=trace,age*,gc*=info:file=${log_home}/gc-%t.log:time,tid,tags:filecount=5,filesize=50m
 "
 # shellcheck disable=SC2086
 exec java ${JAVA_OPTS} -jar app.jar
