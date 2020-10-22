1cb087b5157715c3d536c6699e581448616518ab
libinglong <libinglong9@gmail.com>
2020-10-22 17:42:08 +0800

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 set -e
+logfile=gc.log.$(date "+%Y-%m-%d.%H-%M-%S")
+JAVA_OPTS="
+-Xms2G -Xmx2G -server
+-XX:+UnlockExperimentalVMOptions -XX:+UseZGC
+-Xloggc:${logfile}
+-XX:+PrintGC
+-XX:+PrintReferenceGC
+-XX:+PrintHeapAtGC
+-XX:+PrintGCDetails
+-XX:+PrintGCTimeStamps
+-XX:+PrintGCDateStamps
+-XX:+PrintAdaptiveSizePolicy
+-XX:+PrintGCApplicationStoppedTime
+-XX:+PrintGCApplicationConcurrentTime
+-XX:+PrintTenuringDistribution
+-XX:+SafepointTimeout
+-XX:SafepointTimeoutDelay=1000
+-XX:+PrintSafepointStatistics
+-XX:PrintSafepointStatisticsCount=1
+"
 # shellcheck disable=SC2086
-exec java -jar app.jar
+exec java ${JAVA_OPTS} -jar app.jar
