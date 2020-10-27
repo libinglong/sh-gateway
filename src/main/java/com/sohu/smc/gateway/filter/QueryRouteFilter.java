e5d6f7af34410161aa5f5860bf85ef7dc47d9fa4
libinglong <libinglong9@gmail.com>
2020-10-27 15:50:32 +0800

    up

--- src/main/java/com/sohu/smc/gateway/filter/QueryRouteFilter.java
+++ src/main/java/com/sohu/smc/gateway/filter/QueryRouteFilter.java
@@ -12,6 +12,8 @@ import reactor.core.publisher.Mono;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
 
 /**
+ * 由于路由方式可能是较为复杂的,因此,使用filter来完成路由的查询功能
+ * 当参数中含有{@link QueryRouteFilter#PARAM_NAME},那么则直接返回路由id,而不会转发请求.
  * @author binglongli217932
  * <a href="mailto:libinglong9@gmail.com">libinglong:libinglong9@gmail.com</a>
  * @since 2020/10/27
@@ -19,11 +21,13 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.G
 @Component
 public class QueryRouteFilter implements GlobalFilter, Ordered {
 
+    private static final String PARAM_NAME = "queryForRoute";
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         boolean queryForRoute = exchange.getRequest()
                 .getQueryParams()
-                .containsKey("queryForRoute");
+                .containsKey(PARAM_NAME);
         if (queryForRoute){
             Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
             byte[] bytes = route.getId().getBytes();
