ecee1326b617f8a09c985e01c2d4a838e00c62bd
libinglong <libinglong9@gmail.com>
2020-11-26 14:30:58 +0800

    update storge dir

--- src/test/java/com/sohu/smc/gateway/GatewayApplicationTests.java
+++ src/test/java/com/sohu/smc/gateway/GatewayApplicationTests.java
@@ -1,13 +1,52 @@
 package com.sohu.smc.gateway;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 //@SpringBootTest
 class GatewayApplicationTests {
 
-//    @Test
+    @Test
     void contextLoads() {
+        Flux.range(-2,5)
+                .map(integer -> integer + 100)
+                .map(s -> {
+                    System.out.println("first " + s);
+                    if (s == 100 || s == 109){
+                        throw new RuntimeException("s == 0 error");
+                    }
+                    return s;
+                })
+                .map(integer -> integer + 1000)
+                .map(integer -> integer + 1000)
+                .onErrorContinue((throwable, o) -> {
+                    System.out.println("continue" + o);
+                })
+//                .onErrorResume(throwable -> Flux.range(7,8))
+                .subscribe(integer -> System.out.println("sub " + integer));
+
+    }
+
+    @Test
+    public void fun() throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,Object> map = new HashMap<>();
+        map.put("a", 1);
+        String a = objectMapper.writeValueAsString(map);
+        String b = objectMapper.writeValueAsString("map");
+        System.out.println(a);
+        System.out.println(b);
+
     }
 
 }
